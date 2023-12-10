2d86c74778f902d1a59be52133fc5c19f0bfd8e2
Ayan Sujon <73520526+AyanSujon@users.noreply.github.com>
2023-12-11 01:11:28 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+// #INSIDE OUR APP -> Slider
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+
+// #WHAT OUR CLIENTS SAY’S Slider
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
